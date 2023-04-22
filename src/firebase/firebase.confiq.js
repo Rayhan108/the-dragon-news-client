8c67622a391f205f72305395691e230414804807
Rayhan Shorker <raihanshorker56@gmail.com>
2023-04-22 21:40:44 +0600

    setup react router and install firebase

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBusadwCEr6XEuFfKmD-3y1Gpu69SyBXUs",
+  authDomain: "the-dragon-news-49e22.firebaseapp.com",
+  projectId: "the-dragon-news-49e22",
+  storageBucket: "the-dragon-news-49e22.appspot.com",
+  messagingSenderId: "172596822505",
+  appId: "1:172596822505:web:b053e3aeafe5b690120b78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
