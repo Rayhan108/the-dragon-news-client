8ec8e381755166b37dbbe4f648540f21b89ccdfc
Rayhan Shorker <raihanshorker56@gmail.com>
2023-04-23 18:05:48 +0600

    load catagories data from server and show catagories

--- src/pages/Home/Home/shared/LeftNav/LeftNav.jsx
+++ src/pages/Home/Home/shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const  [catagories,setCatagories] =useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/catagories')
+        .then(res=>res.json())
+        .then(data=>setCatagories(data))
+        .catch(error=>console.log(error))
+    },[])
+    // console.log(catagories);
     return (
+
+
         <div>
-            <h1>left nav</h1>
+            <h4>All Catagories</h4>
+           <div className='mt-4 ps-3 fs-5'>
+           {
+                catagories.map(catagory=><p
+                    key={catagory.id}>
+                    <Link className='text-decoration-none text-black' to={`/catagory/${catagory.id}`}>{catagory.name}</Link>
+                </p>)
+            }
+           </div>
         </div>
     );
 };
